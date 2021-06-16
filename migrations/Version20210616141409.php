f1c68046d86ba57d2a9b41fa7f8320837c1a4f47
Manuel Alejandro Moscoso Dominguez <manuel@mobiquos.cl>
2021-06-16 10:18:42 -0400

    New users files

--- migrations/Version20210616141409.php
+++ migrations/Version20210616141409.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210606162450 extends AbstractMigration
+final class Version20210616141409 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,7 @@ final class Version20210606162450 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE contenido (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(500) NOT NULL, descripcion LONGTEXT NOT NULL, fecha_creacion DATETIME NOT NULL, fecha_actualizacion DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE apunte ADD contenido_id INT NOT NULL');
+        $this->addSql('CREATE TABLE usuario (id INT AUTO_INCREMENT NOT NULL, correo VARCHAR(255) NOT NULL, nombre VARCHAR(255) NOT NULL, codigo VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE apunte ADD CONSTRAINT FK_E39B02327FDA517C FOREIGN KEY (contenido_id) REFERENCES contenido (id)');
         $this->addSql('CREATE INDEX IDX_E39B02327FDA517C ON apunte (contenido_id)');
     }
@@ -29,9 +28,8 @@ final class Version20210606162450 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE usuario');
         $this->addSql('ALTER TABLE apunte DROP FOREIGN KEY FK_E39B02327FDA517C');
-        $this->addSql('DROP TABLE contenido');
         $this->addSql('DROP INDEX IDX_E39B02327FDA517C ON apunte');
-        $this->addSql('ALTER TABLE apunte DROP contenido_id');
     }
 }
