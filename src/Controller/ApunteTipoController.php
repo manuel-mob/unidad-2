ad923e9207735cec7495f7f422be56f78a11a2fd
Manuel Alejandro Moscoso Dominguez <manuel@mobiquos.cl>
2021-06-16 10:34:24 -0400

    Hotfix for tipo de apunte menu

--- src/Controller/ApunteTipoController.php
+++ src/Controller/ApunteTipoController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/apunte/tipo")
+ * @Route("/apuntes/tipos")
  */
 class ApunteTipoController extends AbstractController
 {
